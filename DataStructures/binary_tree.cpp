bb7b90c56bfad843eeb1d20410a37966dbb8003e
artem <artempesterev8@gmail.com>
2021-02-28 21:47:59 +0300

    Fixed binary_tree.cpp

--- DataStructures/binary_tree.cpp
+++ DataStructures/binary_tree.cpp
@@ -4,17 +4,17 @@
 #define BT binary_tree
 
 #define elif else if
-
 #define out(value) printf("%f\n", value)
 
 typedef struct tree2
 {
-	double* self;
+	double self;
+	bool isset;
 	tree2* left_tree;
 	tree2* right_tree;
 } binary_tree;
 
-void init_tree(BT *tree, double *initial)
+void init_tree(BT *tree, double initial)
 {
 	tree->self = initial;
 	tree->left_tree = NULL;
@@ -22,22 +22,43 @@ void init_tree(BT *tree, double *initial)
 	return;
 }
 
-void insert_into_tree(BT *tree, double *value)
+void print_sorted(BT tree)
 {
-	if(tree->self == NULL) // Если текущий элемент неопределён
+	if(tree.isset == false)
+		return;
+	else
 	{
-		tree = (BT*)malloc(sizeof(BT));
-		init_tree(tree, value);
+		if(tree.left_tree != NULL)
+			print_sorted(*tree.left_tree);
+		printf("%f ", tree.self);
+		if(tree.right_tree != NULL)
+			print_sorted(*tree.right_tree);
 		return;
 	}
-	else if(*tree->self == *value) // Если определён, но уже есть в дереве
-		return;
-	else if(*value < *tree->self) // Если определён и меньше вставляемого
+}
+
+void insert_into_tree(BT *tree, double value)
+{
+	if(tree->isset == false) // Если текущий элемент неопределён
 	{
+		init_tree(tree, value);
+		tree->isset = true;
+		return;
+	}
+	else if(tree->self == value) // Если определён, но уже есть в дереве
+		return;
+	else if(value < tree->self) // Если определён и меньше вставляемого
+	{
+		if(tree->left_tree == NULL)
+			tree->left_tree = (BT*)malloc(sizeof(BT));
 		insert_into_tree(tree->left_tree, value);
 	}
 	else // Если определён и больше вставляемого
+	{
+		if(tree->right_tree == NULL)
+			tree->right_tree = (BT*)malloc(sizeof(BT));
 		insert_into_tree(tree->right_tree, value);
+	}
 	return;
 }
 
@@ -45,10 +66,12 @@ int main(void)
 {
 	double A[] = {13,12,15,16,17,51,24,39,48,24};
 	int N = 10;
-	BT my = {NULL, NULL, NULL};
+	BT my = {0, false, NULL, NULL};
 	for(int i = 0; i < 10; i++)
 	{
-		insert_into_tree(&my, A+i);
+		insert_into_tree(&my, A[i]);
 	}
+	print_sorted(my);
+	printf("\n");
 	return 0;
 }
